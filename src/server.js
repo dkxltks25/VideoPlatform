3b51fbd106b51087d6f364d9d9035f875c5717e4
bagjaehong <dkxltks25@naver.com>
2020-05-09 18:10:48 +0900

    .env 파일 추가 및 포트 번호 수정

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
 import {GraphQLServer} from "graphql-yoga";
-
+import morgan from "morgan";
 
 const typeDefs = `
     type Query{
@@ -14,5 +16,5 @@ const resolvers = {
 
 const server = new GraphQLServer({typeDefs,resolvers})
 
-
+server.express.use(morgan("dev"));
 server.start(4000,()=>console.log('serverStart'))
