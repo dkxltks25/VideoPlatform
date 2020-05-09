6073abf6f9b542e7b446496b3cac39349ab039f9
bagjaehong <dkxltks25@naver.com>
2020-05-09 18:12:11 +0900

    기본 설정추가

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ const resolvers = {
     }
 };
 
+const PORT = process.env.PORT || 4000;
 const server = new GraphQLServer({typeDefs,resolvers})
 
 server.express.use(morgan("dev"));
-server.start(4000,()=>console.log('serverStart'))
+server.start(PORT,()=>console.log(`Video Platform Backend api Server Start http://localhost:${PORT}`);
