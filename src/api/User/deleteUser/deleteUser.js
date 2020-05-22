f2818a479701a32eb27fda002232b3343fb54a3d
bagjaehong <dkxltks25@naver.com>
2020-05-23 03:43:43 +0900

    [User] deleteUser Add

--- src/api/User/deleteUser/deleteUser.js
+++ src/api/User/deleteUser/deleteUser.js
@@ -1,14 +1,26 @@
 import { User } from "../../../model";
-import {isAuthenticated} from "../../../middlewares";
+import { isAuthenticated } from "../../../middlewares";
 export default {
   Mutation: {
-    deleteUser: async (_, __,{request}) => {
+    deleteUser: async (_, __, { request }) => {
       isAuthenticated(request);
-      const {user} = {request};
-      try{
-        await User.findByIdAndDelete({_id:user.id});
+      const { user } = request;
+      try {
+        //        await User.findByIdAndDelete({ _id: user.id }, { $pull: {'subscriber':user.id} });
+
+        // const findUser = await User.find({ subscriber: { $in: "5ec80e33a2a16141d3883c24" } });
+
+        const findUser = await User.findByIdAndRemove(
+          { _id: user.id },
+          { subscriber: { $in: "5ec80e33a2a16141d3883c24" } }
+        );
+        findUser.map((findUser) => {
+          findUser.subscriber.pull(user.id);
+          findUser.save();
+        });
+        console.log(findUser);
         return true;
-      }catch(error){
+      } catch (error) {
         console.log(error);
         return false;
       }
